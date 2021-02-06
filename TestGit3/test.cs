cd99414c58c77633b030e4d0165243a21c7a911f
Tarun Nair <tarunsnair09@gmail.com>
2021-02-06 20:25:51 +0530

    Initial commit

---
+++ TestGit3/test.cs
@@ -0,0 +1,4 @@
+using static System.console;
+static void Main(){
+    Writeline("This is just a test to test Git in C#");
+}
--- app.js
+++ app.js
@@ -1,2 +1,2 @@
 console.log("Hey bruh, I'm learning Git!");
-console.log("Watcha learning? CSS? HTML? JavaScript? TypeScript? Go? Ruby? Python? Idk, and I DON'T CARE!");
+console.log("Watcha learning? CSS? HTML? JavaScript? TypeScript? Go? Ruby? Python? C#? H? Idk, and I DON'T CARE!");
